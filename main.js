d202681115a9dd4a2e400b327d654cfa70325b0d
Devesh Dayal <devesh95@users.noreply.github.com>
2015-08-21 15:01:51 +0530

    quick main fix
    - fix handler for POST

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ router.get('/*', function(req, res) {
 });
 
 router.post('/*', function(req, res) {
-	
+	getResponseFromDbOfType('GET', req, res);
 });
 
 var getResponseFromDbOfType = function(type, req, res) {
